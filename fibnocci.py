8122f95227a3091d9a6be7da786398272077857f
sanjeev5-hub <78578737+sanjeev5-hub@users.noreply.github.com>
2022-02-11 13:19:57 +0530

    Update fibnocci.py

--- fibnocci.py
+++ fibnocci.py
@@ -1,9 +1,9 @@
-def sum(n):
+def fib(n):
     if n == 1:
         return 1
     elif n == 0:
         return 0
-    return sum(n -1 ) + sum(n - 2)
+    return fib(n -1 ) + fib(n - 2)
 
-nums = [sum(i) for i in range(10)]
+nums = [fib(i) for i in range(10)]
 print(nums)
